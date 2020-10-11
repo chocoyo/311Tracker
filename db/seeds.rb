0d2edce5cd8ed31c82344794e5223195057dbf28
Mike Hodges <mikehodges7@outlook.com>
2020-10-11 17:26:34 -0500

    Created Seed File And Seeded DB With 200 Records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
 
-# rails g model Call call_time:datetime se
+puts "Deleting Current Data"
+Call.destroy_all
+Neighbourhood.destroy_all
+ServiceArea.destroy_all
+ServiceRequest.destroy_all
+Ward.destroy_all
+
+puts "Parsing File"
+file = Rails.root.join("db/311_Service_Request.csv")
+data = File.read(file)
+calls = CSV.parse(data, headers: true, encoding: "iso-8859-1")
+
+i = 0
+puts "Populating Database"
+calls.each do |call|
+  Call.create(
+    service_area:    ServiceArea.find_or_create_by(service_area: call["Service Area"]),
+    service_request: ServiceRequest.find_or_create_by(service_request: call["Service Request"]),
+    ward:            Ward.find_or_create_by(ward: call["Ward"]),
+    neighbourhood:   Neighbourhood.find_or_create_by(neighbourhood: call["Neighbourhood"])
+  )
+  i += 1
+  if i == 200
+    break
+  end
+end
+
+puts "Seed Compeleted"
