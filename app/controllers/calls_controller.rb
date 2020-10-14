c0b7e3054390a7e09eb35bc062545b78c6ea9dd0
Mike Hodges <mikehodges7@outlook.com>
2020-10-14 13:17:11 -0500

    Add a show call page

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,6 +1,10 @@
 class CallsController < ApplicationController
   def index
-    #Figure out what the N+1 stuff is
+    # Figure out what the N+1 stuff is
     @calls = Call.all
+  end
+
+  def show
+    @call = Call.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "calls/index"
   get "/calls/", to: "calls#index"
+  get "calls/:id", to: "calls#show", as: "call"
 end
