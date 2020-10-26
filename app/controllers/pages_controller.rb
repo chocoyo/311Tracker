9ebcd880293eb03390eef04b9128b11e177d9259
Mike Hodges <mikehodges7@outlook.com>
2020-10-26 14:56:02 -0500

    Added Option To Search By All Calls Or By Service Areas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
   def search
-    if params[:search].present?
+    if params[:search].present? && params[:category].blank?
       @matching_requests = ServiceRequest.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
       @search_results = Call.where(service_request: @matching_requests).page params[:page]
+    elsif params[:category] == "service_area"
+      @matching_areas = ServiceArea.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
+      @search_results = Call.where(service_area: @matching_areas).page params[:page]
     end
   end
 end
