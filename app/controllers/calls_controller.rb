490bad9b80054e42c59b409cbf5907ce294726ff
Mike Hodges <mikehodges7@outlook.com>
2020-10-26 14:17:57 -0500

    Added Blank Search Protection And Added Dropdown For Categories

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,8 +1,10 @@
 class CallsController < ApplicationController
   def index
     # Figure out what the N+1 stuff is
-    #@calls = Call.all
+    # @calls = Call.all
     @calls = Call.all.page params[:page]
+
+    @service_requests = ServiceRequest
   end
 
   def show
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
   def search
-    if params[:search].blank?
-      redirect_to(root_path, alert: "Empty field!") and return
-    else
+    if params[:search].present?
       @matching_requests = ServiceRequest.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
       @search_results = Call.where(service_request: @matching_requests).page params[:page]
     end
