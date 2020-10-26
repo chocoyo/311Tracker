7b84ea7a025c3a1e6823458cf46b5f7010501151
Mike Hodges <mikehodges7@outlook.com>
2020-10-26 15:13:48 -0500

    Cleaned Up Search Code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,22 @@
 class PagesController < ApplicationController
   def search
-    if params[:search].present? && params[:category].blank?
+    if params[:search].blank?
+
+    elsif params[:category].blank? # Need to change this thing, rn its searcjes request on Calls
       @matching_requests = ServiceRequest.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
       @search_results = Call.where(service_request: @matching_requests).page params[:page]
     elsif params[:category] == "service_area"
       @matching_areas = ServiceArea.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
       @search_results = Call.where(service_area: @matching_areas).page params[:page]
+    elsif params[:category] == "service_request"
+      @matching_areas = ServiceArea.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
+      @search_results = Call.where(service_area: @matching_areas).page params[:page]
+    elsif params[:category] == "ward"
+      @matching_wards = Ward.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
+      @search_results = Call.where(ward: @matching_wards).page params[:page]
+    elsif params[:category] == "neighbourhood"
+      @matching_neighbourhoods = Neighbourhood.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
+      @search_results = Call.where(neighbourhood: @matching_neighbourhoods).page params[:page]
     end
   end
 end
