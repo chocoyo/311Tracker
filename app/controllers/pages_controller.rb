e7e8e5b132b022e0d4b6311d5bb7eb9fbf91dbc3
Mike Hodges <mikehodges7@outlook.com>
2020-10-27 21:12:48 -0500

    Changed Search To Filter Options Properly

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,13 @@
 class PagesController < ApplicationController
   def search
-    if params[:search].blank?
+    if params[:search].blank? || params[:category].blank?
 
-    elsif params[:category].blank? # Need to change this thing, rn its searcjes request on Calls
-      @matching_requests = ServiceRequest.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
-      @search_results = Call.where(service_request: @matching_requests).page params[:page]
     elsif params[:category] == "service_area"
       @matching_areas = ServiceArea.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
       @search_results = Call.where(service_area: @matching_areas).page params[:page]
     elsif params[:category] == "service_request"
-      @matching_areas = ServiceArea.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
-      @search_results = Call.where(service_area: @matching_areas).page params[:page]
+      @matching_requests = ServiceRequest.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
+      @search_results = Call.where(service_request: @matching_requests).page params[:page]
     elsif params[:category] == "ward"
       @matching_wards = Ward.where("lower(name) LIKE :search", search: "%#{params[:search].downcase}%")
       @search_results = Call.where(ward: @matching_wards).page params[:page]
